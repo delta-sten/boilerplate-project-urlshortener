e0d034632706d38c0d4d467aedc9ea8e581b5702
Sten Schmidt <sten.schmidt@uscca.com>
2024-06-19 08:34:10 -0500

    k

--- index.js
+++ index.js
@@ -19,6 +19,10 @@ app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.post('/api/shorturl', (req, res) => {
+  console.log('req: ' + req);
+});
+
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
