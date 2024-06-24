d1606333fe1393738f53da812e699f4a37fadf11
Sten Schmidt <sten.schmidt@uscca.com>
2024-06-24 12:47:58 -0500

    h

--- index.js
+++ index.js
@@ -4,6 +4,42 @@ const cors = require('cors');
 const app = express();
 const bodyParser = require('body-parser');
 
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://stenschmidt:w7BUdk8iHGcbEls6@cluster0.ciykdxe.mongodb.net/', { useNewUrlParser: true, useUnifiedTopology: true });
+
+let entry;
+const Schema = mongoose.Schema;
+const urlSchema = new Schema({
+  original_url: {type: String, required: true},
+  short_url: {type: Number, required: true}
+});
+entry = mongoose.model("entry", urlSchema);
+
+
+/*
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+*/
+
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
@@ -33,11 +69,17 @@ app.post('/api/shorturl', (req, res) => {
 
   console.log('URL: ' + url);
   console.log('req.body: ' + req.body);
+  let shortUrlNum = Math.floor(Math.random() * 100000);
+  let newEntry = new entry({original_url: url, short_url: shortUrlNum});
+  newEntry.save(function(err,data) {
+    if (err) {return console.error(err); }
+    document(null, data);
+  });
 
   //let responseOriginal = req.baseUrl;
   res.json({
     original_url: url,
-    short_url: 1
+    short_url: shortUrlNum
   })
 });
 
