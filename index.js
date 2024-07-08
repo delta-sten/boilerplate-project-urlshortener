0ebed1863528dde17d145f442625ba20db70b3c6
Sten Schmidt <sten.schmidt@uscca.com>
2024-07-08 12:19:49 -0500

    g

--- index.js
+++ index.js
@@ -110,11 +110,17 @@ app.get("/api/shorturl/:hash", async (req, res) => {
     console.log("req.params.hash: " + req.params.hash);
     let inputNum = Number(req.params.hash);
     let URLentries = await entry.find({ short_url: inputNum}).exec();
+
     console.log('actual URL: ' + URLentries);
+    for (var key in URLentries) {
+      console.log(key);
+    }
+/*
     console.log('typeof(URLentries): ' + typeof(URLentries));
     console.log('URLentries[1]: ' + URLentries[1]);
     console.log('URLentries.original_url: ' + URLentries.original_url);
     console.log('URLentries["original_url"]: ' + URLentries["original_url"]);
+    */
     res.redirect(URLentries[1]);
   } else {
     res.send({error: 'invalid url'});
